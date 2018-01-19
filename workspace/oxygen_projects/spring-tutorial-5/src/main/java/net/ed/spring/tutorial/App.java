c297428c074f58e4c5d30bce26be3b5b0396bc2e
Edward Dupre <edwd42@gmail.com>
2018-01-19 18:59:46 -0500

    add ClassPathXmlApplicationContext() for beans directory

--- workspace/oxygen_projects/spring-tutorial-5/src/main/java/net/ed/spring/tutorial/App.java
+++ workspace/oxygen_projects/spring-tutorial-5/src/main/java/net/ed/spring/tutorial/App.java
@@ -1,6 +1,7 @@
 package net.ed.spring.tutorial;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class App {
@@ -8,10 +9,14 @@ public class App {
 	public static void main(String[] args) {
 		
 		// Spring bean container
-		ApplicationContext context = new FileSystemXmlApplicationContext("beans.xml");
+		// Change FileSystemXmlApplicationContext to ClassPathXmlApplicationContext
+		ApplicationContext context = new ClassPathXmlApplicationContext("net/ed/spring/tutorial/beans/beans.xml");
 
 		Person person = (Person)context.getBean("person");
 		person.speak();
+		
+		// Prevents resource leaks
+		((ClassPathXmlApplicationContext)context).close();
 	}
 
 }
