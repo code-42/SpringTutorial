010c26d14d85bbc90c00046c65912ec614e994af
Edward Dupre <edwd42@gmail.com>
2018-01-19 17:32:42 -0500

    add App and Person class and Person.speak() method

---
+++ workspace/oxygen_projects/spring-tutorial-5/src/main/java/net/ed/spring/tutorial/App.java
@@ -0,0 +1,11 @@
+package net.ed.spring.tutorial;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		Person person = new Person();
+		person.speak();
+	}
+
+}
---
+++ workspace/oxygen_projects/spring-tutorial-5/src/main/java/net/ed/spring/tutorial/Person.java
@@ -0,0 +1,10 @@
+package net.ed.spring.tutorial;
+
+public class Person {
+	
+	public void speak() {
+		System.out.println("Hello I'm a pseudo person!");
+		
+	}
+
+}
