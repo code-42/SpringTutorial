124c6a937538eb5d82e9a9749fa340e4b3a17750
Edward Dupre <edwd42@gmail.com>
2018-01-19 18:06:30 -0500

    add ApplicationContext container for beans.xml

--- workspace/oxygen_projects/spring-tutorial-5/src/main/java/net/ed/spring/tutorial/App.java
+++ workspace/oxygen_projects/spring-tutorial-5/src/main/java/net/ed/spring/tutorial/App.java
@@ -1,10 +1,16 @@
 package net.ed.spring.tutorial;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
 public class App {
 
 	public static void main(String[] args) {
+		
+		// Spring bean container
+		ApplicationContext context = new FileSystemXmlApplicationContext("beans.xml");
 
-		Person person = new Person();
+		Person person = (Person)context.getBean("person");
 		person.speak();
 	}
 
