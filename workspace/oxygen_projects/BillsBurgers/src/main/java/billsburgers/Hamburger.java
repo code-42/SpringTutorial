2491b8a38ddb25f2ad34011e87597d30be824233
Edward Dupre <edwd42@gmail.com>
2018-01-20 11:25:49 -0500

    add class AdditionalItem with fields, constructor and getters

--- workspace/oxygen_projects/BillsBurgers/src/main/java/billsburgers/Hamburger.java
+++ workspace/oxygen_projects/BillsBurgers/src/main/java/billsburgers/Hamburger.java
@@ -2,6 +2,38 @@ package billsburgers;
 
 public class Hamburger {
 	
+    private String name;
+    private String meat;
+    private String bread;
+    private double price;
+    
+	public Hamburger() {
+	}
 	
+	public Hamburger(String name, String meat, String bread, double price) {
+		this.name = name;
+		this.meat = meat;
+		this.bread = bread;
+		this.price = price;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public String getMeat() {
+		return meat;
+	}
+	public String getBread() {
+		return bread;
+	}
+	public double getPrice() {
+		return price;
+	}
+	@Override
+	public String toString() {
+		return "Hamburger [name=" + name + ", meat=" + meat + ", bread=" + bread + ", price=" + price + "]";
+	}
+    
+    
 
 }
--- workspace/oxygen_projects/BillsBurgers/src/main/java/billsburgers/Main.java
+++ workspace/oxygen_projects/BillsBurgers/src/main/java/billsburgers/Main.java
@@ -3,7 +3,9 @@ package billsburgers;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+        Hamburger basicBurger = new Hamburger("Bill's Basic Burger", "ham", "wheat", 5.00);
+        System.out.println("Here's your " + basicBurger.getName());
 
 	}
 
